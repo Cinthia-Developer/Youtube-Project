7ee23da2b82e20ad7ad54f4bf9d50c15642d1d42
Cinthia Mashiel <cinthiamashiel474@gmail.com>
2017-11-15 16:53:41 -0500

    Se subió solo el archivo index

--- js/main.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){                             
-    $(".videos").youtube(["VqEbCxg2bNI", "GMFewiplIbw", "T7VewKI44rQ", "o1zcAQt4tBc", "t_jHrUE5IOk"]);
-    $(".videos_2").youtube(["WtVRAqTCYek", "Nsqrfu-4rLE", "P-MxBG-q0fI", "0Y_s9LClvmw", "fuOfyCiY-sk"]);
-    $(".videos_3").youtube(["EAOSM5-g83E", "Y1HHBXDL9bg", "YUG2-avqArU", "qcZ2Id41nBE", "mS6nH5Js09U"]);
-});
